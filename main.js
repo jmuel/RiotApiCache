06aa24ab38acc9364943efc5cabbc758b49d9f22
jmuel <jmuel11@gmail.com>
2014-11-11 14:09:55 -0600

    It's Alive!
    I can hit a url and it will actually grab the data, return it and stick it into the mongo cache if it's not already there. If it is it returns what's there.
    
    Now it's time to refactor.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var app = require('express')();
 var request = require('request');
 var _ = require('underscore');
-var jsonStream = require('JSONStream');
 var MongoClient = require('mongodb').MongoClient;
 var uriUtil = require('./uri-query-utils.js');
+var collectionName = 'lol_data';
 
 var key = "44b37d42-2e7e-4006-8f65-34b8ef8ca591";
 
@@ -12,22 +12,51 @@ var db;
 app.get("/api/lol/*", function (req, res) {
     var url = "https://na.api.pvp.net";
     url += req.path + uriUtil.addQuery(_.extend({api_key: key}, req.query));
-    checkMongo(url, req, res);
+    checkCache(url, req, res);
 });
 
 
-var checkMongo = function(url, req, res) {
-    var collection = db.collection('lol_data');
+var checkCache = function (url, req, res) {
+    db.collection(collectionName).findOne({"url": url}, function(err, item) {
 
-    collection.findOne({"url": url}, function (err, item) {
         if (err) throw err;
+
         if (item) {
             console.log(item);
             res.send(item.data);
         }
         else {
+            var requestData;
             console.log('nothing in the cache');
-            req.pipe(request(url)).pipe(res);
+
+            var body = '';
+            requestData = request(url);
+
+            requestData.on('data', function(chunk) {
+                body += chunk;
+            });
+
+            requestData.on('end', function() {
+                var data;
+                try {
+                    data = JSON.parse(body);
+                } catch(err) {
+                    res.statusCode = 400;
+                    return res.end('error' + err.message);
+                }
+
+                res.send(data);
+                var document = {
+                    createdAt: new Date(),
+                    url: url,
+                    data: data
+                };
+
+                db.collection(collectionName).insert(document, function(err) {
+                    if(err) throw err;
+                });
+            });
+
         }
 
     });
@@ -38,6 +67,9 @@ MongoClient.connect('mongodb://127.0.0.1:27017/test', function (err, databaseCon
 
     db = databaseConnection;
 
+    db.collection(collectionName).ensureIndex({createdAt: 1}, {expiresAfterSeconds: 60}, function(err) {
+        if(err) throw err;
+    });
 
 });
 
