4209b6d8944d0a2075bfc930f1866091a2edf499
jmuel <jmuel11@gmail.com>
2014-11-11 15:09:14 -0600

    Doing a few refactors.

--- main.js
+++ main.js
@@ -5,20 +5,20 @@ var MongoClient = require('mongodb').MongoClient;
 var uriUtil = require('./uri-query-utils.js');
 var collectionName = 'lol_data';
 
-var key = "44b37d42-2e7e-4006-8f65-34b8ef8ca591";
+var key = '44b37d42-2e7e-4006-8f65-34b8ef8ca591';
+var mongoUrl = 'mongodb://127.0.0.1:27017/test';
+var riotBaseUrl = "https://na.api.pvp.net";
 
 var db;
 
 app.get("/api/lol/*", function (req, res) {
-    var url = "https://na.api.pvp.net";
-    url += req.path + uriUtil.addQuery(_.extend({api_key: key}, req.query));
-    checkCache(url, req, res);
+    var url = riotBaseUrl + req.path + uriUtil.addQuery(_.extend({api_key: key}, req.query));
+    checkCache(url, res);
 });
 
 
-var checkCache = function (url, req, res) {
+var checkCache = function (url, res) {
     db.collection(collectionName).findOne({"url": url}, function(err, item) {
-
         if (err) throw err;
 
         if (item) {
@@ -26,48 +26,55 @@ var checkCache = function (url, req, res) {
             res.send(item.data);
         }
         else {
-            var requestData;
-            console.log('nothing in the cache');
-
-            var body = '';
-            requestData = request(url);
-
-            requestData.on('data', function(chunk) {
-                body += chunk;
-            });
-
-            requestData.on('end', function() {
-                var data;
-                try {
-                    data = JSON.parse(body);
-                } catch(err) {
-                    res.statusCode = 400;
-                    return res.end('error' + err.message);
-                }
-
-                res.send(data);
-                var document = {
-                    createdAt: new Date(),
-                    url: url,
-                    data: data
-                };
-
-                db.collection(collectionName).insert(document, function(err) {
-                    if(err) throw err;
-                });
-            });
+            hitRiotAndStoreItInACache(url, res);
 
         }
 
     });
+};
+
+var hitRiotAndStoreItInACache = function (url, res) {
+    var requestData;
+    console.log("hitting riot directly");
+
+    var body = '';
+    requestData = request(url);
+
+    requestData.on('data', function(chunk) {
+        body += chunk;
+    });
+
+    requestData.on('end', function() {
+        var data;
+        try {
+            data = JSON.parse(body);
+        } catch(err) {
+            res.statusCode = 400;
+            return res.end('error' + err.message);
+        }
+
+        res.send(data);
+        var document = {
+            createdAt: new Date(),
+            url: url,
+            data: data
+        };
+
+        db.collection(collectionName).insert(document, function(err) {
+            if(err) throw err;
+        });
+    });
 }
 
-MongoClient.connect('mongodb://127.0.0.1:27017/test', function (err, databaseConnection) {
+
+
+
+MongoClient.connect(mongoUrl, function (err, databaseConnection) {
     if (err) throw err;
 
     db = databaseConnection;
 
-    db.collection(collectionName).ensureIndex({createdAt: 1}, {expiresAfterSeconds: 60}, function(err) {
+    db.collection(collectionName).ensureIndex({createdAt: 1}, {expireAfterSeconds: 60}, function(err) {
         if(err) throw err;
     });
 
